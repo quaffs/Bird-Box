c46b3f494e50ec7d4ab24a50a4224922376a7bca
Brady Anderson <bradya48@gmail.com>
2020-01-04 17:19:03 -0600

    create and add logo to home

--- UI/bird-box/src/App.vue
+++ UI/bird-box/src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/test">Test</router-link>
     </div>
     <router-view />
   </div>
@@ -25,7 +26,7 @@
     color: #2c3e50;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: #e91313;
     }
   }
 }
--- UI/bird-box/src/views/Home.vue
+++ UI/bird-box/src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Bird Box</h1>
+    <img id="logo" alt="Vue logo" src="../assets/logo/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App!" /> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
+  name: "home"
+  // name: "home",
+  // components: {
+  //   HelloWorld
+  // }
 };
 </script>
+
+<style scoped lang="scss">
+#logo {
+  height: 275px;
+}
+</style>
