ba01cf36704f95ebdaad26f891e0a87f5c33f443
Brady Anderson <bradya48@gmail.com>
2020-01-10 14:16:05 -0600

    add sound wave display

---
+++ UI/bird-box/src/components/Wave.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="wave">
+    <h3>Wave</h3>
+    <div id="waveform"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import WaveSurfer from "wavesurfer.js";
+Vue.nextTick(() => {
+  console.log("next tick");
+  var wavesurfer = WaveSurfer.create({
+    container: "#waveform",
+    waveColor: "red",
+    progressColor: "black"
+  });
+  wavesurfer.load("northern-cardinal-dvg1.wav");
+});
+
+@Component
+export default class Wave extends Vue {
+  @Prop() private msg!: string;
+  onClick(): void {
+    window.alert("Hello World");
+  }
+}
+</script>
--- UI/bird-box/src/views/Home.vue
+++ UI/bird-box/src/views/Home.vue
@@ -3,19 +3,21 @@
     <h1>Bird Box</h1>
     <img id="logo" alt="Vue logo" src="../assets/logo/logo.png" />
     <!-- <HelloWorld msg="Welcome to Your Vue.js App!" /> -->
+    <Wave />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from "@/components/HelloWorld.vue";
+import Wave from "@/components/Wave.vue";
 
 export default {
-  name: "home"
-  // name: "home",
-  // components: {
-  //   HelloWorld
-  // }
+  // name: "home"
+  name: "home",
+  components: {
+    Wave
+  }
 };
 </script>
 
