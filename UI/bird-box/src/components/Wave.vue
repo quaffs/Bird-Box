37a1d1b12c981cfa93e3938c95a677632a3ed9cc
Brady Anderson <bradya48@gmail.com>
2020-01-15 10:41:26 -0600

    fix soundwave display

--- UI/bird-box/src/components/Wave.vue
+++ UI/bird-box/src/components/Wave.vue
@@ -8,21 +8,23 @@
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 import WaveSurfer from "wavesurfer.js";
-Vue.nextTick(() => {
-  console.log("next tick");
-  var wavesurfer = WaveSurfer.create({
-    container: "#waveform",
-    waveColor: "red",
-    progressColor: "black"
-  });
-  wavesurfer.load("northern-cardinal-dvg1.wav");
-});
 
 @Component
 export default class Wave extends Vue {
   @Prop() private msg!: string;
-  onClick(): void {
-    window.alert("Hello World");
+  // this runs when the component is loaded
+  mounted() {
+    this.loadWave();
+  }
+
+  // this loads the wave form
+  loadWave() {
+    var wavesurfer = WaveSurfer.create({
+      container: "#waveform",
+      waveColor: "red",
+      progressColor: "black"
+    });
+    wavesurfer.load("northern-cardinal-dvg1.wav");
   }
 }
 </script>
