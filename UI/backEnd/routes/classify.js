fb3db8cd327975e5905ba2ea0da41a35128c7092
Brady Anderson <bradya48@gmail.com>
2020-02-04 23:54:59 -0600

    add skeleton for mock response

--- UI/backEnd/routes/classify.js
+++ UI/backEnd/routes/classify.js
@@ -35,15 +35,40 @@ var fileUpload = upload.fields([
 router.post("/uploadForm", fileUpload, function(req, res, next) {
   // create ID for process and create directory
   var id = shortid.generate();
-  var result = { id: id };
+  var results = { id: id, classifyResults: [] };
   var curAudioDir = audioInputDir + id;
   console.log(`Upload Form Route Reached (id: ${id})`);
   if (!fs.existsSync(curAudioDir)) fs.mkdirSync(curAudioDir);
   // process files
   var processResult = processFiles(req.files, curAudioDir);
-  Object.assign(result, processResult);
-  res.json(result);
+  // make sure all results were process, then run algorithms
+  if (!processResult.error) {
+    for (var i = 0; i < processResult.fileCount; i++) {
+      // run classify script for all files at same time
+      var classifications = [];
+      for (var j = 0; j < processResult.segResults.length; j++) {
+        // send to classification algorithm
+        classifications.push(classify(path.join(curAudioDir, `file${j}Segs`, `seg${i.toString().padStart(3, '0')}.wav`)));
+      }
+      // send to direction and distance algorithm
+      results.classifyResults.push(distanceAndDirection(classifications));
+    }
+      
+    // send ca
+  }
+  // run classification algorithm for each call
+  
+  // for (var file in req.files) {
+  //   var filePath =
+  //   classifications.push(filePath);
+  // }
+
+  // run direction/distance algorithm
+  Object.assign(results, processResult);
+  res.json(results);
 });
+
+/* POST mock response */
 
 /* POST upload sound. 
    This demonstrates how a the upload sound route can be reached */
@@ -110,7 +135,8 @@ router.post("/responseExample", function(req, res, next) {
 });
 
 function processFiles(files, audioDir) {
-  response = { msg: "", error: "" };
+  var response = { segResults: [], error: false, errorMsg: "", fileCount: 0 };
+  var fileCounts = [];
   for (const key in files) {
     file = files[key][0];
     // move file to directory
@@ -118,8 +144,24 @@ function processFiles(files, audioDir) {
     fs.renameSync(file.path, `${newPath}.wav`);
     // segment file
     segmentDir = `${newPath}Segs`;
-    return segmentFiles(`${newPath}.wav`, segmentDir, 10);
+    response.segResults.push(segmentFiles(`${newPath}.wav`, segmentDir, 10));
+    // console.log(path.join(audioDir, `${key}Segs`))
+    fileCounts.push(fs.readdirSync(path.join(audioDir, `${key}Segs`)).length);
   }
+  // make sure files process correctly
+  if (!resultsProcessedCorrect(response.segResults)) {
+    response.error = true;
+    response.errorMsg = "Files were not properly processed";
+    return response;
+  }
+  // make sure file count is correct
+  if (!(fileCounts.every((val, i, arr) => val === arr[0]))) {
+    response.error = true;
+    response.errorMsg = "Files did not have same length";
+    return response;
+  }
+  response.fileCount = fileCounts[0];
+  return response;
 }
 
 function segmentFiles(filePath, outputDir, length) {
@@ -143,6 +185,24 @@ function segmentFiles(filePath, outputDir, length) {
     success: process.status === 0 ? true : false,
     processError: process.stderr.toString()
   };
+}
+
+function resultsProcessedCorrect(segResults) {
+  result = true;
+  segResults.forEach(r => {
+    if (!r.success)
+      result = false;
+  });
+  return result;
+}
+
+function classify(filePath) {
+  return 1;
+}
+
+function distanceAndDirection(classification) {
+  var results = {classification: "", distance: 0, direction: "", timestamp: ""};
+  return results;
 }
 
 module.exports = router;
