807ea9ceebb5c6e6e8a3cf3395dcbbd60000b85a
Ryan Parker <32305537+quaffs@users.noreply.github.com>
2020-02-06 01:40:29 -0600

    Update and rename BirdCallTest.py to BirdCallML.py

--- BirdCallML.py
+++ BirdCallML.py
@@ -6,12 +6,13 @@ from sklearn.model_selection import train_test_split
 
 cardcall = np.load('CardCalls.npy')
 noncall = np.load('NonCalls.npy')
+robcall = np.load('RobinCalls.npy')
 
 #print(cardcall, noncall)
-call = np.vstack([cardcall, noncall])
+call = np.vstack([cardcall, noncall, robcall])
 
 ones = np.ones((149,), dtype=int)
-zeros = np.zeros((147,), dtype=int)
+zeros = np.zeros((147+200,), dtype=int)
 numbers = np.hstack([ones, zeros])
 
 X = np.array(call)
@@ -21,7 +22,7 @@ y = np.array(numbers) # 1 = Cardinal, 0 = NOT Cardinal
 #clf = svm.SVC()
 #clf.fit(X, y)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05, random_state=41)
 clf = tree.DecisionTreeClassifier().fit(X_train, y_train)
 m = clf.score(X_test, y_test)
 
