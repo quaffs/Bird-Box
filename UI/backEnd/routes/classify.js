2da1b4ee8e5f07cd4948c9f4f37969ba3e46b77e
Brady Anderson <bradya48@gmail.com>
2020-02-20 01:05:17 -0600

    remove extra code from route

--- UI/backEnd/routes/classify.js
+++ UI/backEnd/routes/classify.js
@@ -9,31 +9,13 @@ var path = require("path");
 
 var audioInputDir = "./public/audio/";
 
-/* GET classify listing. */
-router.get("/", function(req, res, next) {
-  res.send("respond with a resource");
-});
-
-/* GET example process. 
-   This demonstrates how a python process can be started from node*/
-router.get("/exampleProcess", function(req, res, next) {
-  var spawn = require("child_process").spawn;
-  // runs a process.  Including arguments
-  var process = spawn("python3", ["./HelloWorld.py", "Test Param"]);
-  // send data from output of spawned process to route
-  process.stdout.on("data", function(data) {
-    res.send(data.toString());
-  });
-});
-
 /* Post upload form */
 var fileUpload = upload.fields([
   { name: "file1" },
   { name: "file2" },
   { name: "file3" },
   { name: "file4" },
-  { name: "file5" },
-
+  { name: "file5" }
 ]);
 router.post("/uploadForm", fileUpload, function(req, res, next) {
   // create ID for process and create directory
@@ -51,84 +33,26 @@ router.post("/uploadForm", fileUpload, function(req, res, next) {
       var classifications = [];
       for (var j = 0; j < processResult.segResults.length; j++) {
         // send to classification algorithm
-        classifications.push(classify(path.join(curAudioDir, `file${j}Segs`, `seg${i.toString().padStart(3, '0')}.wav`)));
+        classifications.push(
+          classify(
+            path.join(
+              curAudioDir,
+              `file${j}Segs`,
+              `seg${i.toString().padStart(3, "0")}.wav`
+            )
+          )
+        );
       }
       // send to direction and distance algorithm
       classifySingleResult = distanceAndDirection(classifications, i);
       if (classifySingleResult.classification != "") {
         results.classifyResults.push(classifySingleResult);
-      }  
+      }
     }
   }
   // run direction/distance algorithm
   Object.assign(results, processResult);
   res.json(results);
-});
-
-/* POST mock response */
-
-/* POST upload sound. 
-   This demonstrates how a the upload sound route can be reached */
-router.post("/uploadSound", function(req, res, next) {
-  var id = shortid.generate(); // id for process
-  console.log(`Upload Sound Route Reached (id: ${id})`);
-  // audio file should be saved locally
-  var curAudioDir = audioInputDir + id;
-  if (!fs.existsSync(curAudioDir)) fs.mkdirSync(curAudioDir);
-  fileName = "CardinalCallTest.wav";
-  // create process to segment audio into 10 second segments
-  var processData, processError;
-  var process = spawn("ffmpeg", [
-    "-i",
-    audioInputDir + fileName,
-    "-f",
-    "segment",
-    "-segment_time",
-    "10",
-    "-c",
-    "copy",
-    curAudioDir + "out%03d.wav"
-  ]);
-  process.stdout.on("data", function(data) {
-    processData = data.toString();
-  });
-  process.stderr.on("data", function(error) {
-    processError = error.toString();
-  });
-  process.stdout.on("end", function() {
-    res.json({
-      data: processData,
-      error: processError
-    });
-  });
-});
-
-/* POST response example. 
-   This demonstrates how a the upload sound route can be reached */
-router.post("/responseExample", function(req, res, next) {
-  console.log("Name:", req.body.name);
-  console.log("Message:", req.body.message);
-  exampleJSON = {};
-  exampleJSON.fileName = "testFile.wav";
-  exampleJSON.success = true;
-  exampleJSON.results = {
-    0: {
-      classification: "cardinal",
-      timeStart: 5,
-      timeEnd: 10
-    },
-    1: {
-      classification: "blue jay",
-      timeStart: 13,
-      timeEnd: 20
-    },
-    2: {
-      classification: "cardinal",
-      timeStart: 31,
-      timeEnd: 37
-    }
-  };
-  res.json(exampleJSON);
 });
 
 function processFiles(files, audioDir) {
@@ -151,7 +75,7 @@ function processFiles(files, audioDir) {
     return response;
   }
   // make sure file count is correct
-  if (!(fileCounts.every((val, i, arr) => val === arr[0]))) {
+  if (!fileCounts.every((val, i, arr) => val === arr[0])) {
     response.error = true;
     response.errorMsg = "Files did not have same length";
     return response;
@@ -186,14 +110,13 @@ function segmentFiles(filePath, outputDir, length) {
 function resultsProcessedCorrect(segResults) {
   result = true;
   segResults.forEach(r => {
-    if (!r.success)
-      result = false;
+    if (!r.success) result = false;
   });
   return result;
 }
 
 function classify(filePath) {
-  var result = {callDetected: -1, classification: "null"};
+  var result = { callDetected: -1, classification: "null" };
   var process = child.spawnSync("python3", ["./classify.py", filePath]);
   if (process.status === 0) {
     var lines = process.stdout.toString().split("\n");
@@ -204,22 +127,30 @@ function classify(filePath) {
 }
 
 function distanceAndDirection(classifications, i) {
-  var result = {classification: "", distance: 0, direction: "", timeStart: 0, timeEnd: 0};
+  var result = {
+    classification: "",
+    distance: 0,
+    direction: "",
+    timeStart: 0,
+    timeEnd: 0
+  };
   var classificationArray = [];
   classifications.forEach(e => {
-    if (e.classification != "null")
-      result.classification = e.classification;
+    if (e.classification != "null") result.classification = e.classification;
     classificationArray.push(e.callDetected);
   });
   classificationArraySum = classificationArray.reduce((a, b) => a + b, 0);
   if (classificationArraySum > 0) {
-    var process = child.spawnSync("python3", ["./distanceAndDirection.py", classificationArray.join(" ")]);
+    var process = child.spawnSync("python3", [
+      "./distanceAndDirection.py",
+      classificationArray.join(" ")
+    ]);
     if (process.status === 0) {
       var lines = process.stdout.toString().split("\n");
       result.distance = parseInt(lines[0]);
       result.direction = lines[1];
-      result.timeStart = i*10;
-      result.timeEnd = i*10+10;
+      result.timeStart = i * 10;
+      result.timeEnd = i * 10 + 10;
     }
   }
   return result;
