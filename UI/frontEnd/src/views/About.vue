addbfeaa70ffc3f97dd398079d6d481bfe5467f2
megan griffin <megangr1997@yahoo.com>
2020-02-20 16:13:47 -0600

    Added image and info to the about page.

--- UI/frontEnd/src/views/About.vue
+++ UI/frontEnd/src/views/About.vue
@@ -1,54 +1,11 @@
 <template>
   <div class="about">
-    <h1>All about Bird-Box</h1>
+    <h3>About Bird-Box</h3>
     <br /><br />
-    <h3>Meet the Team</h3>
-    <div class="card-group">
-      <div class="card">
-        <img class="card-img-top" />
-        <div class="card-body">
-          <h5 class="card-title">Brady Anderson</h5>
-          <p class="card-text">
-            Brady is Brady.
-          </p>
-        </div>
-      </div>
-      <div class="card">
-        <img class="card-img-top" />
-        <div class="card-body">
-          <h5 class="card-title">John Do</h5>
-          <p class="card-text">
-            This is John.
-          </p>
-        </div>
-      </div>
-      <div class="card">
-        <img class="card-img-top" />
-        <div class="card-body">
-          <h5 class="card-title">Megan Griffin</h5>
-          <p class="card-text">
-            This is Megan.
-          </p>
-        </div>
-      </div>
-      <div class="card">
-        <img class="card-img-top" />
-        <div class="card-body">
-          <h5 class="card-title">Ryan Parker</h5>
-          <p class="card-text">
-            This is Ryan.
-          </p>
-        </div>
-      </div>
-      <div class="card">
-        <img class="card-img-top" />
-        <div class="card-body">
-          <h5 class="card-title">Tracey Samanie</h5>
-          <p class="card-text">
-            This is Tracey.
-          </p>
-        </div>
-      </div>
-    </div>
+    <h5>
+      The following image shows the setup of the microphones used to collect our
+      bird call samples.
+    </h5>
+    <img src="../assets/micDiagram.png" height="500" />
   </div>
 </template>
